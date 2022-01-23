d68e8156791aa349357900b0fd6d79e24110502a
Josh Morgan <eston1007@gmail.com>
2022-01-22 18:17:17 -0800

    some refactoring and also testing some new functionality

--- main.py
+++ main.py
@@ -2,56 +2,64 @@ import discord
 import random
 import sys
 from discord.ext import commands
-import os
 
 bot = commands.Bot(command_prefix="!")
 
+
 @bot.event
 async def on_ready():
-  print("we have logged in as {0.user}.format(client)")
+    print("we have logged in as {0.user}.format(client)")
+
 
 @bot.command()
 async def scramble(ctx, *args):
-  print("here")
 
-  if int(args[0]) > 10:
-    await ctx.send("No more than 10 teams, ask Rob why")
-    return
+    if int(args[0]) > 10:
+        await ctx.send("No more than 10 teams, ask Rob why")
+        return
 
-  if len(args) > 100:
-    await ctx.send("No more than 100 players, ask Rob why")
-    return
-  
-  number_of_teams = args[0]
-  players = list()
-  
+    if len(args) > 100:
+        await ctx.send("No more than 100 players, ask Rob why")
+        return
 
-  for i in range(1,len(args)):
-    players.append(args[i])
+    number_of_teams = args[0]
+    players = list()
 
-  if (len(players) % int(number_of_teams) != 0):
-    await ctx.send("Not able to make teams with even numbers")
-    return
+    for i in range(1, len(args)):
+        players.append(args[i])
 
-  player_in_team = len(players) / int(number_of_teams)
-  
-  teams = list()
-  for i in range(0, int(number_of_teams)):
-    print("num of teams")
-    temp_team = list()
-    for x in range(0, int(player_in_team)):
-      print("players in team")
-      temp_team.append(players.pop(random.randrange(0, len(players))))
-    teams.append(temp_team)
+    if (len(players) % int(number_of_teams) != 0):
+        await ctx.send("Not able to make teams with even numbers")
+        return
 
-  number = 1  
-  for team in teams:
-    team_string = "**Team** " + str(number) + "\n"
-    
+    player_in_team = len(players) / int(number_of_teams)
+
+    teams = list()
+    for i in range(0, int(number_of_teams)):
+        temp_team = list()
+        for x in range(0, int(player_in_team)):
+            temp_team.append(players.pop(random.randrange(0, len(players))))
+        teams.append(temp_team)
+
+    number = 1
+    for team in teams:
+        team_string = "**Team** " + str(number) + "\n"
+
     for member in team:
-      team_string += member + "\n"
+        team_string += member + "\n"
     number += 1
     await ctx.send(team_string)
 
-bot.run(os.getenv("TOKEN"))
+
+@bot.command()
+async def test(ctx, *args):
+    channel = discord.utils.get(ctx.guild.channels, name=args[0])
+    members = channel.members
+    for member in members:
+        await ctx.send(member.name)
+    print("Testing")
+
+token_file = open("token.txt", "r")
+token = token_file.readline()
+bot.run(token)
 sys.stdout.flush()
